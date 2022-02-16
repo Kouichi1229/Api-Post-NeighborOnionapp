c2721d155644ddb44bd75c4d8d8b9ba812a43cf4
K <jpr64914@gmail.com>
2022-02-16 00:08:19 -0800

    update0216

--- app.py
+++ app.py
@@ -18,12 +18,6 @@ if page == '栽植期':
         rainMsg: str = st.text_area('雨量注意事項(若沒有可不填)',max_chars=150)
 
         data={
-        'update_time':datetime.datetime(
-            year=update_time.year,
-            month=update_time.month,
-            day=update_time.day,
-            hour=0,
-            minute=0).isoformat(),
         'temMsg': temMsg,
         'rainMsg': rainMsg
         }
@@ -31,14 +25,13 @@ if page == '栽植期':
         submit_btn=st.form_submit_button(label='送出')
 
     if submit_btn:
-        st.write('## 送出資料')
-        st.json(data)
-        st.write('## 結果')
-        url='http://stifmm.deta.dev/Remind/plant'
+        url = 'http://127.0.0.1:8000/users'
         res = requests.post(
             url,
             data=json.dumps(data)
         )
+        if res.status_code == 200:
+            st.success('送出資料')
         st.json(res.json())
 else:
     st.title("隔壁老洋-預警報撰寫畫面")
@@ -49,26 +42,19 @@ else:
         alercontent: str = st.text_area('預警報',max_chars=400)
 
         data={
-        'update_time':datetime.datetime(
-            year=update_time.year,
-            month=update_time.month,
-            day=update_time.day,
-            hour=0,
-            minute=0).isoformat(),
-        'alercontent': alercontent,
+        'alercontent': alercontent
         }
 
         submit_btn=st.form_submit_button(label='送出')
 
     if submit_btn:
-        st.write('## 送出資料')
-        st.json(data)
-        st.write('## 結果')
-        url='http://stifmm.deta.dev/Remind/aler'
+        url = 'http://127.0.0.1:8000/rooms'
         res = requests.post(
             url,
             data=json.dumps(data)
         )
+        if res.status_code == 200:
+            st.success('送出資料')
         st.json(res.json())
         
         
