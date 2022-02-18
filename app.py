9bacff9b4d9a7da453a389d0d96085e24cd53b92
K <jpr64914@gmail.com>
2022-02-17 18:32:42 -0800

    fianl

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
-import  streamlit as st
+import streamlit as st
+import datetime 
 import requests
 import json
-import datetime
+import pandas as pd
 
 page = st.sidebar.selectbox('選擇頁面',['栽植期','預警報'])
 
-
-
 if page == '栽植期':
-    st.title("隔壁老洋-栽植期撰寫畫面")
-    with  st.form(key='PlantMsg'):
+    st.title('隔壁老洋-栽植期撰寫畫面')
+    with st.form(key='user'):
+        # user_id: int = random.randint(0, 10)
         update_time = st.date_input(
         "更新日期",
         min_value=datetime.date.today())
-        
         temMsg: str = st.text_area('溫度注意事項(若沒有可不填)',max_chars=150)
         rainMsg: str = st.text_area('雨量注意事項(若沒有可不填)',max_chars=150)
-
-        data={
-        'temMsg': temMsg,
-        'rainMsg': rainMsg
+        data = {
+            
+            'temp': temMsg,
+            'rain':rainMsg
         }
+        submit_button = st.form_submit_button(label='送出')
 
-        submit_btn=st.form_submit_button(label='送出')
-
-    if submit_btn:
-        url = 'http://127.0.0.1:8000/users'
+    if submit_button:
+        url = 'https://apipost-no-v1.herokuapp.com/users'
         res = requests.post(
             url,
             data=json.dumps(data)
@@ -33,22 +31,26 @@ if page == '栽植期':
         if res.status_code == 200:
             st.success('送出資料')
         st.json(res.json())
-else:
-    st.title("隔壁老洋-預警報撰寫畫面")
-    with  st.form(key='AlerMsg'):
+
+elif page == '預警報':
+    st.title('隔壁老洋-預警報撰寫畫面')
+
+    with st.form(key='room'):
+        # room_id: int = random.randint(0, 10)
         update_time = st.date_input(
         "更新日期",
         min_value=datetime.date.today())
         alercontent: str = st.text_area('預警報',max_chars=400)
-
-        data={
-        'alercontent': alercontent
+        
+        data = {
+            # 'room_id': room_id,
+            'aler': alercontent
+            
         }
+        submit_button = st.form_submit_button(label='送出')
 
-        submit_btn=st.form_submit_button(label='送出')
-
-    if submit_btn:
-        url = 'http://127.0.0.1:8000/rooms'
+    if submit_button:
+        url = 'https://apipost-no-v1.herokuapp.com/rooms'
         res = requests.post(
             url,
             data=json.dumps(data)
@@ -56,6 +58,9 @@ else:
         if res.status_code == 200:
             st.success('送出資料')
         st.json(res.json())
-        
-        
-       
+
+
+
+
+
+
